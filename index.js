c930138950f4788c8262d9ec031e79a739255f47
jrcliff <justoon7.jc@gmail.com>
2020-11-18 17:35:59 -0500

    commit tree 49d519a2985ac231adebf3d1ed23b496ea54f6d3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ['Ada', 'Brendan', 'Ali']
+let eventName = 'birthday'
+function writeCards(names, eventName){
+    let cards = []
+    for (let i = 0; i < names.length; i++){
+
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cards
+}
+
+function countDown(num){
+    for (let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
